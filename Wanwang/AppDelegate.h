0d65738a416a6be8c5266e910e5979c4a8085ffb
keo <sunmeo@kaka-5.local>
2012-11-28 09:37:29 +0800

    Initial Commit

---
+++ Wanwang/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Wanwang
+//
+//  Created by keo on 11/28/12.
+//  Copyright (c) 2012 keo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
